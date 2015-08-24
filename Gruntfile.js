425b2f6c7d2082cd227069517fd931678567a98d
Rachel Carden <bamadesigner@gmail.com>
2015-08-23 20:52:44 -0500

    Adding Grunt into the mix

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        sass: {
+            wordcampus: {
+                options: {
+                    style: 'compressed',
+                    noCache: true,
+                    sourcemap: 'none'
+                },
+                files: [{
+                    expand: true,
+                    src: '*.scss',
+                    cwd: 'scss',
+                    dest: 'css',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        watch: {
+            wordcampus: {
+                files: [ '**/*' ],
+                tasks: [ 'newer:sass:wordcampus' ]
+            }
+        }
+    });
+
+    // Load our dependencies
+    grunt.loadNpmTasks( 'grunt-contrib-sass' );
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+    grunt.loadNpmTasks( 'grunt-newer' );
+
+    // Register our tasks
+   	grunt.registerTask( 'default', [ 'newer:sass', 'watch' ] );
+
+    // Register a watch function
+    grunt.event.on( 'watch', function( action, filepath, target ) {
+        grunt.log.writeln( target + ': ' + filepath + ' has ' + action );
+    });
+
+};
