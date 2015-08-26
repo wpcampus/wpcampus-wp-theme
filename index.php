7a7049a4d1058e342d58199957564164b3d06393
Rachel Carden <bamadesigner@gmail.com>
2015-08-25 23:08:43 -0500

    Adding the loop to the general template

--- index.php
+++ index.php
@@ -2,4 +2,18 @@
 
 get_header();
 
+if ( have_posts() ) {
+    while ( have_posts() ) {
+        the_post();
+
+        if ( ! is_front_page() ) {
+            ?><h1><?php the_title(); ?></h1><?php
+        }
+
+        the_content();
+
+    }
+
+}
+
 get_footer();
