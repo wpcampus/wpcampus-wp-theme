ad4a5cd625ea879274554f501d5fc5497e62d74c
Rachel Carden <bamadesigner@gmail.com>
2016-01-26 10:44:05 -0600

    Updating the GitHub URL now that WPCampus has a GitHub account.

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
                 <div class="addthis_sharing_toolbox"></div>
                 <p>WPCampus is an effort to create a new community and conference for those using WordPress in the world of higher education. If you have any ideas, comments, or questions, please share them using <a href="https://wpcampus.org/contact/">our contact form</a>.</p>
                 <p>If our community and/or a WordPress conference for higher education is something you would be interested in (whether it’s as an attendee, speaker, planner, sponsor, or all of the above), please <a href="<?php echo get_bloginfo('url'); ?>">submit our Get Involved form</a> (and share with your friends) to show your support.</p>
-                <p class="github-message">This site is powered by <a href="https://wordpress.org/">WordPress</a>. You can view, and contribute to, the theme on <a href="https://github.com/bamadesigner/wordcampus-wp-theme">GitHub</a>.</p>
+                <p class="github-message">This site is powered by <a href="https://wordpress.org/">WordPress</a>. You can view, and contribute to, the theme on <a href="https://github.com/wpcampus/wpcampus-wp-theme">GitHub</a>.</p>
                 <p class="twitter"><a href="https://twitter.com/wpcampusorg"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter-black.svg" /></a></p>
             </div>
         </div>
