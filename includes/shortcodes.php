f6c2031d6490d3d017de2d803bf22139e63682cc
Rachel Carden <bamadesigner@gmail.com>
2016-02-06 20:36:53 -0600

    Removing extra space from around/within shortcodes

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,11 +1,26 @@
 <?php
 
+// We have to remove the default shortcode filter
+remove_filter( 'the_content', 'do_shortcode', 11 ); // AFTER wpautop()
+
+// Strips extra space around and within shortcodes
+add_filter( 'the_content', function( $content ) {
+
+    // Clean it
+    $content = strtr( $content, array( "\n[" => '[', "]\n" => ']', '<p>[' => '[', ']</p>' => ']', ']<br>' => ']', ']<br />' => ']' ) );
+
+    // Return the content
+    return do_shortcode( $content );
+
+}, 11 );
+
 //! Include columns in content
 add_shortcode( 'columns', function( $args, $content = NULL ) {
 
     // Make sure there's content to wrap
-    if ( ! $content )
-        return NULL;
+    if ( ! $content ) {
+		return NULL;
+	}
 
     // Process for more levels of shortcode, wrap in row and return
     return '<div class="row">' . do_shortcode( $content ) . '</div>';
@@ -16,8 +31,9 @@ add_shortcode( 'columns', function( $args, $content = NULL ) {
 add_shortcode( 'col', function( $args, $content = NULL ) {
 
     // Make sure there's content to wrap
-    if ( ! $content )
-        return NULL;
+    if ( ! $content ) {
+		return NULL;
+	}
 
     // Process args
     $defaults = array(
