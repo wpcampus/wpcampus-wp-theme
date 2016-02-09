b210afa6f17b32052c6c34730e4387d1b3d5b294
Rachel Carden <bamadesigner@gmail.com>
2016-02-08 23:29:59 -0600

    Adding the WPCampus logo to, and updating, the footer

--- footer.php
+++ footer.php
@@ -6,9 +6,10 @@
         <div class="row">
             <div class="small-12 columns">
                 <div class="addthis_sharing_toolbox"></div>
-                <p>WPCampus is an effort to create a new community and conference for those using WordPress in the world of higher education. If you have any ideas, comments, or questions, please share them using <a href="https://wpcampus.org/contact/">our contact form</a>.</p>
-                <p>If our community and/or a WordPress conference for higher education is something you would be interested in (whether it’s as an attendee, speaker, planner, sponsor, or all of the above), please <a href="<?php echo get_bloginfo('url'); ?>/get-involved/">submit our Get Involved form</a> (and share with your friends) to show your support.</p>
-                <p class="github-message">This site is powered by <a href="https://wordpress.org/">WordPress</a>. You can view, and contribute to, the theme on <a href="https://github.com/wpcampus/wpcampus-wp-theme">GitHub</a>.</p>
+                <a class="wpc-logo"  href="https://wpcampus.org/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wpcampus-logo-tagline.svg" alt="WPCampus: Where WordPress Meets Higher Education" /></a>
+                <p><strong>WPCampus is a community and <a href="https://2016.wpcampus.org/" title="Learn more about the WPCampus conference">conference</a> for those using WordPress in the world of higher education.</strong><br />
+                If you are not already a member of the WPCampus community, we hope you'll join us and <a href="https://wpcampus.org/get-involved/">learn more on how to get involved</a>.<br />
+                <span class="github-message">This site is powered by <a href="https://wordpress.org/">WordPress</a>. You can view, and contribute to, the theme on <a href="https://github.com/wpcampus/wpcampus-wp-theme">GitHub</a>.</span></p>
                 <p class="icons">
                     <a class="twitter" href="https://twitter.com/wpcampusorg"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter-black.svg" alt="Follow WPCampus on Twitter" /></a>
                     <a class="github" href="https://github.com/wpcampus/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/github-black.svg" alt="Follow WPCampus on GitHub" /></a>
