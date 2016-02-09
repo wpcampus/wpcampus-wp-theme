3a22e8e55b62a6af51ed8da22d67123e6dabd1f6
Rachel Carden <bamadesigner@gmail.com>
2016-02-08 21:38:20 -0600

    Added GitHub icon to the header and footer

--- footer.php
+++ footer.php
@@ -9,7 +9,10 @@
                 <p>WPCampus is an effort to create a new community and conference for those using WordPress in the world of higher education. If you have any ideas, comments, or questions, please share them using <a href="https://wpcampus.org/contact/">our contact form</a>.</p>
                 <p>If our community and/or a WordPress conference for higher education is something you would be interested in (whether it’s as an attendee, speaker, planner, sponsor, or all of the above), please <a href="<?php echo get_bloginfo('url'); ?>/get-involved/">submit our Get Involved form</a> (and share with your friends) to show your support.</p>
                 <p class="github-message">This site is powered by <a href="https://wordpress.org/">WordPress</a>. You can view, and contribute to, the theme on <a href="https://github.com/wpcampus/wpcampus-wp-theme">GitHub</a>.</p>
-                <p class="twitter"><a href="https://twitter.com/wpcampusorg"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter-black.svg" alt="Follow WPCampus on Twitter" /></a></p>
+                <p class="icons">
+                    <a class="twitter" href="https://twitter.com/wpcampusorg"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter-black.svg" alt="Follow WPCampus on Twitter" /></a>
+                    <a class="github" href="https://github.com/wpcampus/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/github-black.svg" alt="Follow WPCampus on GitHub" /></a>
+                </p>
             </div>
         </div>
     </div> <!-- #wpcampus-footer -->
