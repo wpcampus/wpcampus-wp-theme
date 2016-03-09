d844121b16da62f5112f4db9096dd16f153d0b27
Rachel Carden <rmcarden@ua.edu>
2016-03-09 13:10:25 -0600

    Tweaking the name of the podcast guide in the admin

--- includes/admin.php
+++ includes/admin.php
@@ -7,7 +7,7 @@ function wpcampus_add_meta_boxes( $post_type, $post ) {
 	// Add a meta box to link to the podcast guide
 	add_meta_box(
 		'wpcampus-podcast-guide',
-		'Creating a WPCampus Podcast',
+		'WPCampus Podcast Guide',
 		'wpcampus_print_meta_boxes',
 		'podcast',
 		'side',
@@ -21,7 +21,7 @@ function wpcampus_print_meta_boxes( $post, $metabox ) {
 	switch( $metabox[ 'id' ] ) {
 
 		case 'wpcampus-podcast-guide':
-			?><div style="background:rgba(0,115,170,0.07);padding:18px;color:#000;margin:-6px -12px -12px -12px;">Be sure to read our <a href="https://docs.google.com/document/d/1GG8-qb4OQ3TzDyB1UI00GvRw-agyIO1AT8WUPuyDgHg/edit#heading=h.8dr748uym2qn" target="_blank">Guide For Creating a WPCampus Podcast</a> to help walk you through the process and ensure proper setup.</div><?php
+			?><div style="background:rgba(0,115,170,0.07);padding:18px;color:#000;margin:-6px -12px -12px -12px;">Be sure to read our <a href="https://docs.google.com/document/d/1GG8-qb4OQ3TzDyB1UI00GvRw-agyIO1AT8WUPuyDgHg/edit#heading=h.8dr748uym2qn" target="_blank">WPCampus Podcast Guide</a> to help walk you through the process and ensure proper setup.</div><?php
 			break;
 
 	}
