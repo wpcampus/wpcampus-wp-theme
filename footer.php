ac7e483ec754aee45e729e46932baf7a4c613d27
Rachel Carden <bamadesigner@gmail.com>
2016-10-18 18:30:07 -0700

    Adding a footer menu

--- footer.php
+++ footer.php
@@ -7,6 +7,10 @@
             <div class="small-12 columns">
                 <div class="addthis_sharing_toolbox"></div>
                 <a class="wpc-logo"  href="https://wpcampus.org/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wpcampus-logo-tagline.svg" alt="WPCampus: Where WordPress Meets Higher Education" /></a>
+                <ul class="wpc-footer-menu" role="navigation">
+                    <li><a href="https://wpcampus.org/code-of-conduct/">Code of Conduct</a></li>
+                    <li><a href="https://wpcampus.org/contact/">Contact Us</a></li>
+                </ul>
                 <p><strong>WPCampus is a community and <a href="https://2016.wpcampus.org/" title="Learn more about the WPCampus conference">conference</a> for those using WordPress in the world of higher education.</strong><br />
                 If you are not already a member of the WPCampus community, we hope you'll join us and <a href="https://wpcampus.org/get-involved/">learn more on how to get involved</a>.<br />
                 <span class="github-message">This site is powered by <a href="https://wordpress.org/">WordPress</a>. You can view, and contribute to, the theme on <a href="https://github.com/wpcampus/wpcampus-wp-theme">GitHub</a>.</span></p>
