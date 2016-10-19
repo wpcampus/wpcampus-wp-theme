56d97166f40950fb4d988f49ef14547a19da3bb8
Rachel Carden <bamadesigner@gmail.com>
2016-10-18 18:30:29 -0700

    Adding a conferences submenu

--- header.php
+++ header.php
@@ -34,7 +34,13 @@ $is_events_page = is_post_type_archive('tribe_events') || is_singular('tribe_eve
             <ul>
                 <li class="icon has-icon-alt home<?php echo $is_front_page ? ' current' : null; ?>"><a href="<?php echo $blog_url; ?>"><img src="<?php echo $stylesheet_dir; ?>/images/home-white.svg" alt="Visit the WPCampus home page" /><span class="icon-alt">Home</span></a></li>
                 <li<?php echo is_page( 'get-involved' ) ? ' class="current"' : null; ?>><a href="<?php echo $blog_url; ?>/get-involved/">Get Involved</a></li>
-                <li><a href="https://2016.wpcampus.org/">Conference</a></li>
+                <li class="has-submenu"><a href="<?php echo $blog_url; ?>/conferences/">Conferences</a>
+                    <ul>
+	                    <li><a href="<?php echo $blog_url; ?>/online/">WPCampus Online</a></li>
+	                    <li><a href="<?php echo $blog_url; ?>/apply-to-host/">Apply to host 2017</a></li>
+                        <li><a href="https://2016.wpcampus.org/">WPCampus 2016</a></li>
+                    </ul>
+                </li>
                 <li<?php echo is_post_type_archive('podcast') || is_singular('podcast') ? ' class="current"' : null; ?>><a href="<?php echo $blog_url; ?>/podcast/">Podcast</a></li>
                 <li<?php echo $is_events_page ? ' class="current"' : null; ?>><a href="<?php echo $blog_url; ?>/events/">Events</a></li>
                 <li<?php echo is_page( 'contact' ) ? ' class="current"' : null; ?>><a href="<?php echo $blog_url; ?>/contact/">Contact</a></li>
