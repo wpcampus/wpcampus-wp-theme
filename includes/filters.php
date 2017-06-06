1eb2141884aa8a43b2b4cf73a971636f71ae37b3
Rachel Carden <Rachel.Carden@disney.com>
2017-06-05 22:25:22 -0700

    Cleaning up the filters file

--- includes/filters.php
+++ includes/filters.php
@@ -1,11 +1,11 @@
 <?php
-	
+
 /**
  * Remove the auto <p> when needed.
  */
 function wpcampus_remove_filters() {
-	
-	// Remove on order t-shirt page
+
+	// Remove on order t-shirt page.
 	if ( is_page( 'order-wpcampus-shirt' ) ) {
 		remove_filter( 'the_content', 'wpautop' );
 	}
@@ -15,59 +15,59 @@ add_action( 'wp', 'wpcampus_remove_filters' );
 /**
  * Filter the page title.
  */
-add_filter( 'wpcampus_page_title', function( $page_title ) {
+function wpcampus_filter_page_title( $page_title ) {
 
-	/**
-	 * Change the title for events pages.
+	/*
+	 * Change the page title.
 	 *
-	 * Had to write in because events plugin was
-	 * overwriting the 'post_type_archive_title' filter.
+	 * Had to write in for events because the
+	 * events plugin was overwriting the
+	 * 'post_type_archive_title' filter.
 	 */
-	if ( is_post_type_archive('tribe_events') || is_singular('tribe_events') ) {
-		return 'Events';
-	}
-
-	// Change the title for the main podcast page
-	else if ( is_post_type_archive('podcast') ) {
-		return 'WPCampus Podcast';
-	}
-
-	// Prefix the title for the single podcast pages
-	else if ( is_singular('podcast') ) {
-		return '<span class="fade">Podcast:</span> ' . $page_title;
+	if ( is_post_type_archive( 'tribe_events' ) || is_singular( 'tribe_events' ) ) {
+		return __( 'Events', 'wpcampus' );
+	} elseif ( is_post_type_archive( 'podcast' ) ) {
+		return sprintf( __( 'The %s Podcast', 'wpcampus' ), 'WPCampus' );
+	} elseif ( is_singular( 'podcast' ) ) {
+		return '<span class="fade">' . __( 'Podcast:', 'wpcampus' ) . '</span> ' . $page_title;
 	}
 
 	return $page_title;
-});
+}
+add_filter( 'wpcampus_page_title', 'wpcampus_filter_page_title' );
 
 /**
  * Filter the post type archive title.
  */
-add_filter( 'wpcampus_post_type_archive_title', function( $title, $post_type ) {
+function wpcampus_filter_post_type_archive_title( $title, $post_type ) {
 
-	// Had to write in because events plugin was overwriting the 'post_type_archive_title' filter
-	if ( is_post_type_archive('tribe_events') || is_singular('tribe_events') ) {
-		return 'Events';
+	/*
+	 * Had to write in because events plugin was
+	 * overwriting the 'post_type_archive_title' filter.
+	 */
+	if ( is_post_type_archive( 'tribe_events' ) || is_singular( 'tribe_events' ) ) {
+		return __( 'Events', 'wpcampus' );
 	}
 
-	if ( is_post_type_archive('podcast') ) {
-		return 'Podcast';
+	if ( is_post_type_archive( 'podcast' ) ) {
+		return __( 'Podcast', 'wpcampus' );
 	}
 
 	return $title;
-}, 100, 2 );
+}
+add_filter( 'wpcampus_post_type_archive_title', 'wpcampus_filter_post_type_archive_title', 100, 2 );
 
 /**
  * Filter/build our own map infowindow content.
  */
-add_filter( 'gmb_mashup_infowindow_content', function( $response, $marker_data, $post_id ) {
+function wpcampus_filter_gmb_mashup_infowindow_content( $response, $marker_data, $post_id ) {
 
-	// Only for interest posts
+	// Only for interest posts.
 	if ( 'wpcampus_interest' != get_post_type( $marker_data['id'] ) ) {
 		return $response;
 	}
 
-	// Build new infowindow content
+	// Build new infowindow content.
 	$response['infowindow'] = '<div id="infobubble-content" class="main-place-infobubble-content">';
 
 	// Get location
@@ -76,9 +76,9 @@ add_filter( 'gmb_mashup_infowindow_content', function( $response, $marker_data, 
 		$response['infowindow'] .= '<p class="place-title">' . $location . '</p>';
 	}
 
-	// Close the window element
+	// Close the window element.
 	$response['infowindow'] .= '</div>';
 
 	return $response;
-
-}, 100, 3 );
+}
+add_filter( 'gmb_mashup_infowindow_content', 'wpcampus_filter_gmb_mashup_infowindow_content', 100, 3 );
