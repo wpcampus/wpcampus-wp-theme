0c2f1e24cd65fa0d530ff1ef3cce4dfaba61fb45
Rachel Carden <Rachel.Carden@disney.com>
2017-07-02 17:46:47 -0700

    Optimizing notification styles

--- functions.php
+++ functions.php
@@ -120,7 +120,7 @@ add_action( 'wp_loaded', 'wpcampus_add_category_to_podcast' );
  * Setup styles and scripts.
  */
 function wpcampus_enqueue_styles_scripts() {
-	$wpcampus_version = '0.69';
+	$wpcampus_version = '0.72';
 
 	// Get the directory.
 	$wpcampus_dir = trailingslashit( get_stylesheet_directory_uri() );
