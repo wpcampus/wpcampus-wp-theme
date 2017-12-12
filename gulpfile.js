9c101870cf507c4bf1e3dd3f7b8c1457efb651b3
Rachel Carden <Rachel.Carden@disney.com>
2017-12-12 14:33:42 -0800

    Adding SASS to the gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,30 @@
-// Require all the things (that we need).
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 const gulp = require('gulp');
+const mergeMediaQueries = require('gulp-merge-media-queries');
+const normalize = require('node-normalize-scss').includePaths;
 const notify = require('gulp-notify');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 const shell = require('gulp-shell');
 const sort = require('gulp-sort');
 const uglify = require('gulp-uglify');
-const watch = require('gulp-watch');
 const wp_pot = require('gulp-wp-pot');
+
+// Define our SASS includes.
+const sassIncludes = [].concat(normalize);
 
 // Define the source paths for each file type.
 const src = {
 	js: ['assets/js/wpcampus.js', 'assets/js/wpcampus-data.js', 'assets/js/wpcampus-sessions.js'],
-	php: ['**/*.php','!vendor/**','!node_modules/**']
+	php: ['**/*.php','!vendor/**','!node_modules/**'],
+	sass: ['assets/scss/**/*']
 };
 
+// Define the destination paths for each file type.
 const dest = {
 	js: 'assets/js',
+	sass: 'assets/css',
 	translations: 'languages/wpcampus-wp-theme.pot'
 };
 
@@ -30,6 +39,28 @@ gulp.task('js', function() {
 		}))
         .pipe(gulp.dest(dest.js))
 		.pipe(notify('WPC Main JS compiled'));
+});
+
+// Take care of SASS.
+gulp.task('sass', function() {
+	return gulp.src(src.sass)
+		.pipe(sass({
+			includePaths: sassIncludes,
+			outputStyle: 'expanded'
+		}).on('error', sass.logError))
+		.pipe(mergeMediaQueries())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(cleanCSS({
+			compatibility: 'ie8'
+		}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(dest.sass))
+		.pipe(notify('WPC Main SASS compiled'));
 });
 
 // "Sniff" our PHP.
@@ -62,16 +93,17 @@ gulp.task('translate', function () {
 		.pipe(notify('WPC Main translated'));
 });
 
-// Compile all the things.
-gulp.task('compile',['js']);
-
-// Test all the things.
+// Test our files.
 gulp.task('test',['php']);
+
+// Compile all the things.
+gulp.task('compile',['sass','js']);
 
 // I've got my eyes on you(r file changes).
 gulp.task('watch', function() {
 	gulp.watch(src.js, ['js']);
-	gulp.watch(src.php, ['translate','php']);
+	gulp.watch(src.php,['php','translate']);
+	gulp.watch(src.sass,['sass']);
 });
 
 // Let's get this party started.
