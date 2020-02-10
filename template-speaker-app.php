27e73a6f5fab83479ff2729df6713e1d8863b30d
Rachel Cherry <bamadesigner@gmail.com>
2020-02-10 00:47:58 -0500

    Adding speaker app JS to speaker app template

--- template-speaker-app.php
+++ template-speaker-app.php
@@ -12,7 +12,11 @@ function wpcampus_speaker_app_assets() {
 	// Load the counter part to the iframe resizer.
 	wp_enqueue_script( 'wpc-iframe-resizer', trailingslashit( get_stylesheet_directory_uri() ) . 'assets/js/iframeResizer.contentWindow.min.js', array(), null );
 
+	// Is registered in speakers plugin.
+	wp_enqueue_script( 'wpc-speaker-app' );
+
 }
+
 add_action( 'wp_enqueue_scripts', 'wpcampus_speaker_app_assets', 20 );
 
 get_template_part( 'template-content-only' );
